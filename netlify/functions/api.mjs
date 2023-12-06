7fad41f5ea7936fc21f2e6e1e7da1c545a6283ac
Fyrok1 <tahsincesur1@gmail.com>
2023-12-07 00:06:51 +0300

    netlify init

---
+++ netlify/functions/api.mjs
@@ -0,0 +1,40 @@
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
+
+// import { createCanvas } from "canvas";
+
+// app.get("/", (req, res) => {
+//   const canvas = createCanvas(1000, 200);
+//   const ctx = canvas.getContext("2d");
+//   ctx.font = "30px Impact";
+//   ctx.rotate(0.1);
+//   ctx.fillText("Awesome!", 50, 100);
+
+//   // Draw line under text
+//   var text = ctx.measureText("Awesome!");
+//   ctx.strokeStyle = "rgba(0,0,0,0.5)";
+//   ctx.beginPath();
+//   ctx.lineTo(50, 102);
+//   ctx.lineTo(50 + text.width, 102);
+//   ctx.stroke();
+//   // console.log('<img src="' + canvas.toDataURL() + '" />');
+
+//   res.writeHead(200, {
+//     "Content-Type": "image/png",
+//   });
+
+//   res.end(canvas.toBuffer("image/png"));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
