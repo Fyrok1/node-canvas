25506cb475ffec8b29eab5337816aa83b53edb50
Fyrok1 <tahsincesur1@gmail.com>
2023-12-07 00:08:58 +0300

    netlify add image path

--- netlify/functions/api.mjs
+++ netlify/functions/api.mjs
@@ -5,6 +5,30 @@ const api = express();
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/image", (req, res) => {
+  const { createCanvas } = require("canvas");
+  const canvas = createCanvas(1000, 200);
+  const ctx = canvas.getContext("2d");
+  ctx.font = "30px Impact";
+  ctx.rotate(0.1);
+  ctx.fillText("Awesome!", 50, 100);
+
+  // Draw line under text
+  var text = ctx.measureText("Awesome!");
+  ctx.strokeStyle = "rgba(0,0,0,0.5)";
+  ctx.beginPath();
+  ctx.lineTo(50, 102);
+  ctx.lineTo(50 + text.width, 102);
+  ctx.stroke();
+  // console.log('<img src="' + canvas.toDataURL() + '" />');
+
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+  });
+
+  res.end(canvas.toBuffer("image/png"));
+});
+
 
 api.use("/api/", router);
 
